0f875b64ed61635d31419b1c2287eac6c77cc68f
dengchao <dc134565@alibaba-inc.com>
2017-05-17 09:25:02 +0800

    finish the test case

--- src/demos/nms_demo/nms_main.cpp
+++ src/demos/nms_demo/nms_main.cpp
@@ -10,20 +10,28 @@
 int main(int argc, char* argv[])
 {
 	// prepare the picture and the targets windows, and display
-	cv::Rect detect_window1(10, 10, 60, 70);
-	cv::Rect detect_window2(11, 12, 60, 70);
-	cv::Rect detect_window3(13, 15, 60, 70);
+	cv::Rect detect_window1(10, 10, 260, 270);
+	cv::Rect detect_window2(19, 19, 260, 270);
+	cv::Rect detect_window3(13, 21, 260, 270);
 
-	cv::Rect detect_window4(70, 80, 90, 70);
-	cv::Rect detect_window5(75, 88, 89, 70);
-	cv::Rect detect_window6(65, 75, 98, 70);
-	cv::Rect detect_window7(80, 70, 90, 79);
+	cv::Rect detect_window4(470, 280, 270, 270);
+	cv::Rect detect_window5(475, 288, 270, 270);
+	cv::Rect detect_window6(465, 275, 270, 270);
+	cv::Rect detect_window7(480, 270, 270, 270);
 
-	cv::Rect detect_window8(100, 30, 60, 70);
-	cv::Rect detect_window9(110, 32, 60, 70);
+	cv::Rect detect_window8(200, 530, 200, 170);
+	cv::Rect detect_window9(210, 532, 200, 170);
 
-	cv::Mat image(400, 400, CV_8UC3, cv::Scalar(0));
+	cv::Mat image(800, 800, CV_8UC3, cv::Scalar(0));
 	cv::rectangle(image, detect_window1, cv::Scalar(0, 255, 255));
+	cv::rectangle(image, detect_window2, cv::Scalar(0, 255, 255));
+	cv::rectangle(image, detect_window3, cv::Scalar(0, 255, 255));
+	cv::rectangle(image, detect_window4, cv::Scalar(255, 0, 255));
+	cv::rectangle(image, detect_window5, cv::Scalar(255, 0, 255));
+	cv::rectangle(image, detect_window6, cv::Scalar(255, 0, 255));
+	cv::rectangle(image, detect_window7, cv::Scalar(255, 0, 255));
+	cv::rectangle(image, detect_window8, cv::Scalar(255, 255, 0));
+	cv::rectangle(image, detect_window9, cv::Scalar(255, 255, 0));
 
 	cv::imshow("rectangles", image);
 	cv::waitKey();
